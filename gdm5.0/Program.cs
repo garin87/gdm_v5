a54c0d5ca3ecbddef756ee7360f4a391e553a5be
kamrader <robertcash610@gmail.com>
2026-05-12 10:50:53 +0200

    Refactoring

--- gdm5.0/Program.cs
+++ gdm5.0/Program.cs
@@ -1,12 +1,5 @@
-using gdm5._0.Services;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace gdm5._0
 {
